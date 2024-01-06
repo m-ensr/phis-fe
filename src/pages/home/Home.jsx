d370c320d8e3da4ff1ef11fa0c092093c4df6d90
m-ensr <ensar.ozdemir@yahoo.com>
2024-01-06 17:34:33 +0300

    Update homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,24 @@
-import Header from '../../components/header/Header'
-import Rods from '../../components/rods/Rods'
-import Sidebar from '../../components/sidebar/Sidebar'
-import './home.css'
+import Header from "../../components/header/Header";
+import { Link } from "react-router-dom";
+import "./home.css";
+import Footer from "../../components/footer/footer";
 
 function Home() {
   return (
     <>
-    <Header/>
-    <div className='home'>
-      <Rods/>
-      <Sidebar/>
-    </div>
+      <Header />
+      <div className="home">
+        <Link to="/hunting">
+          <button class="glowing-btn">
+            <span class="glowing-txt">
+              START<span class="faulty-letter"> PHI</span>SHING
+            </span>
+          </button>
+        </Link>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
