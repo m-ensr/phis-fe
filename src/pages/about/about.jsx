f17ee59a63f4aadc6620395a4e8dbb2b09e8839d
m-ensr <ensar.ozdemir@yahoo.com>
2024-01-07 10:12:10 +0300

    Update about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import "./about.css";
 
-function about() {
+function About() {
+  const githubAvatarUrl =
+    "https://avatars.githubusercontent.com/u/94220649?v=4";
+  const githubUrl = "https://github.com/m-ensr";
+  const linkedinUrl = "https://www.linkedin.com/in/m-ensar-ozdemir/";
+
   return (
-    <div>about</div>
-  )
+    <div className="about-container">
+      <div className="card">
+        <img src={githubAvatarUrl} alt="GitHub User 1" />
+        <p>Mustafa Ensar Özdemir</p>
+        <div className="icons">
+          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="icon" />
+          </a>
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+            <FaGithub className="icon" />
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <img src={githubAvatarUrl} alt="GitHub User 2" />
+        <p>Mustafa Ensar Özdemir</p>
+        <div className="icons">
+          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="icon" />
+          </a>
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+            <FaGithub className="icon" />
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <img src={githubAvatarUrl} alt="GitHub User 3" />
+        <p>Mustafa Ensar Özdemir</p>
+        <div className="icons">
+          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="icon" />
+          </a>
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+            <FaGithub className="icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default about
+export default About;
