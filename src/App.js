aadeab9d46c135f7f347c5a5feedd43f006ad912
m-ensr <ensar.ozdemir@yahoo.com>
2024-01-07 09:15:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hunting from './pages/hunting';
+import FacebookLogin from './pages/Facebook/FacebookLogin';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
-
+import About from './pages/about/about';
 
 function App() {
   return (
-   <>
-   <Topbar/>
-   <Home/>
-   </>
+    <Router>
+      <Topbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hunting" element={<Hunting/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/facebook" element={<FacebookLogin />} />
+      </Routes>
+    </Router>
   );
 }
 
