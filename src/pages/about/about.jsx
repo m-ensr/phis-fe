06ac43cb994acb5622ae30a9b08becd2dc85f2bc
m-ensr <ensar.ozdemir@yahoo.com>
2024-01-11 00:47:38 +0300

    Update about.jsx

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -14,9 +14,9 @@ const githubUrl1 = "https://github.com/mteksahin";
 const linkedinUrl1 = "https://www.linkedin.com/in/mteksahin/";
 
 const githubAvatarUrl2 =
-"https://avatars.githubusercontent.com/u/94220649?v=4";
-const githubUrl2 = "https://github.com/m-ensr";
-const linkedinUrl2 = "https://www.linkedin.com/in/m-ensar-ozdemir/";
+"https://media.licdn.com/dms/image/D4D03AQGHCthKJPgzmA/profile-displayphoto-shrink_400_400/0/1672231982431?e=1710374400&v=beta&t=DaBQRUxTjdHJ6KGk2mSMyRdxM--ntQLNH19_r-MEt28";
+const githubUrl2 = "https://github.com/zeynepkiral";
+const linkedinUrl2 = "https://www.linkedin.com/in/zeynepk%C4%B1ral";
   return (
     <div className="about-container">
       <div className="card">
@@ -44,13 +44,13 @@ const linkedinUrl2 = "https://www.linkedin.com/in/m-ensar-ozdemir/";
         </div>
       </div>
       <div className="card">
-        <img src={githubAvatarUrl} alt="GitHub User 3" />
-        <p>Mustafa Ensar Özdemir</p>
+        <img src={githubAvatarUrl2} alt="GitHub User 3" />
+        <p>Zeynep Kıral</p>
         <div className="icons">
-          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+          <a href={linkedinUrl2} target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="icon" />
           </a>
-          <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+          <a href={githubUrl2} target="_blank" rel="noopener noreferrer">
             <FaGithub className="icon" />
           </a>
         </div>
