11c958a7e891e12678da1b1f63d7aa483463aa91
m-ensr <ensar.ozdemir@yahoo.com>
2024-01-13 10:43:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hunting from './pages/hunting';
-import FacebookLogin from './pages/Facebook/FacebookLogin';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import About from './pages/about/about';
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hunting" element={<Hunting/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/facebook" element={<FacebookLogin />} />
       </Routes>
     </Router>
   );
